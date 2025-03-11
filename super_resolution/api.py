b24f93c402e497d4a498b44de6c2f74fb1eb6bd5
Gillian <Gillian.L.Liu@outlook.com>
2025-03-11 10:49:25 +0100

    Revert "tryout3"
    This reverts commit a3bdb203eec1159b7b2dbaef570f3fc01f3cac1b.

--- super_resolution/api.py
+++ super_resolution/api.py
@@ -5,12 +5,6 @@ A Python API for the EMDiffuse Super-Resolution model that processes data
 import os
 import urllib
 import zipfile
-from pathlib import Path
-
-current_directory = Path.cwd()  # Get the current directory
-folders = [folder for folder in current_directory.iterdir() if folder.is_dir()]
-
-print(folders)
 from .models import create_EMDiffuse
 from .core import praser as Praser
 from emdiffuse_conifg import EMDiffuseConfig
